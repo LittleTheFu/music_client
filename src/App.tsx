17e5285e3219accd53681692be1668356c2c6f5c
fuyuxiang <lttlstpd@live.com>
2020-01-27 18:19:24 +0800

    introduce material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <header className="App-header">
+                <Button variant="contained" color="primary">
+                    你好，世界
+                </Button>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
