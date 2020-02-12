660daf5ec6df55923dffcb397b8917a1fcd32a61
fuyuxiang <lttlstpd@live.com>
2020-02-12 21:50:14 +0800

    try indicator

--- src/components/musicComponent.tsx
+++ src/components/musicComponent.tsx
@@ -1,13 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
 const audioElement = new Audio('http://localhost:9999/music');
+
+const useStyles = makeStyles({
+    root: {
+        height: 300,
+    },
+    icon: {
+        size: 800,
+    },
+    box: {
+        height: 100,
+        color: 'red',
+    },
+    thumbers: {
+        color: 'green',
+    },
+    indicator: {
+        left: '0%',
+    },
+});
 
 const MusicComponent: React.FC = () => {
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
     const [percent, setPercent] = useState(0);
+
+    const classes = useStyles({});
 
     useEffect(() => {
         if (duration !== 0) {
@@ -17,6 +39,11 @@ const MusicComponent: React.FC = () => {
 
     const logMsg = (): void => {
         console.log(currentTime + '...' + audioElement.currentTime);
+    };
+
+    const clickProgress = (e: object): void => {
+        // console.log('progress bar clicked');
+        console.log(e);
     };
 
     audioElement.onloadedmetadata = (): void => {
@@ -59,7 +86,16 @@ const MusicComponent: React.FC = () => {
             <h1>{currentTime}</h1>
             <h1>{duration}</h1>
             <h1>{percent}</h1>
-            <LinearProgress variant="determinate" value={percent} />
+            <div>
+                <LinearProgress
+                    variant="determinate"
+                    value={percent}
+                    onClick={(e): void => clickProgress(e)}
+                ></LinearProgress>
+                <Button variant="contained" color="primary" className={classes.indicator}>
+                    icon
+                </Button>
+            </div>
         </div>
     );
 };
