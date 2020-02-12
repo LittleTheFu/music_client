6a24888df85bb35a46d003ebb95959cb212e18f0
fuyuxiang <lttlstpd@live.com>
2020-02-12 21:22:19 +0800

    remove button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
 import MusicComponent from './components/musicComponent';
 
 const App: React.FC = () => {
     return (
         <div>
             <MusicComponent></MusicComponent>
-            <Button variant="contained" color="primary">
-                你好，世界
-            </Button>
         </div>
     );
 };
