18a45c9e6aeffc399931ce5196a2033ad0ca5c59
fuyuxiang <lttlstpd@live.com>
2020-02-25 18:43:28 +0800

    print jwt token

--- src/service.tsx
+++ src/service.tsx
@@ -57,7 +57,8 @@ export const createBlogPost = (data: object): Promise<object> => {
         },
     })
         .then(response => {
-            console.log(response);
+            return response.json();
         })
+        .then(data => console.log(data))
         .catch(err => err);
 };
