33b8f42391e7c2086d2459249a034d83a7b9bcc3
fuyuxiang <lttlstpd@live.com>
2020-03-15 17:05:36 +0800

    reactn debug tool

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import addReactNDevTools from 'reactn-devtools';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+addReactNDevTools();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
