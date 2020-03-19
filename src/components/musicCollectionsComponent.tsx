0caa4699556decf99907c8b7c5becc686a189694
fuyuxiang <lttlstpd@live.com>
2020-03-19 15:46:44 +0800

    fix gridList width

--- src/components/musicCollectionsComponent.tsx
+++ src/components/musicCollectionsComponent.tsx
@@ -13,44 +13,18 @@ interface MusicCollectionsProps {
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        main: {
-            display: 'flex',
-            flexWrap: 'wrap',
-        },
         root: {
             display: 'flex',
             flexWrap: 'wrap',
             justifyContent: 'space-around',
             overflow: 'hidden',
+            width: 600,
+            padding: 20,
         },
         gridList: {
             flexWrap: 'nowrap',
             // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
             // transform: 'translateZ(0)',
-        },
-
-        rooter: {
-            padding: 3,
-        },
-        card: {
-            [theme.breakpoints.down('sm')]: {
-                width: 100,
-            },
-            [theme.breakpoints.up('md')]: {
-                width: 200,
-            },
-        },
-        cover: {
-            [theme.breakpoints.down('sm')]: {
-                width: 75,
-                height: 75,
-            },
-            [theme.breakpoints.up('md')]: {
-                width: 150,
-                height: 150,
-            },
-
-            // borderRadius: '50%',
         },
     }),
 );
@@ -60,17 +34,19 @@ export const MusicCollectionsComponent: React.FC<MusicCollectionsProps> = (props
     const classes = useStyles({});
 
     return (
-        <GridList className={classes.gridList} cols={2.5}>
-            {collections.map((c: MusicCollection, index: number) => {
-                return (
-                    <MusicCollectionComponent
-                        bodyClick={bodyClick}
-                        coverClick={coverClick}
-                        key={index}
-                        collection={c}
-                    ></MusicCollectionComponent>
-                );
-            })}
-        </GridList>
+        <div className={classes.root}>
+            <GridList className={classes.gridList} cols={2.5}>
+                {collections.map((c: MusicCollection, index: number) => {
+                    return (
+                        <MusicCollectionComponent
+                            bodyClick={bodyClick}
+                            coverClick={coverClick}
+                            key={index}
+                            collection={c}
+                        ></MusicCollectionComponent>
+                    );
+                })}
+            </GridList>
+        </div>
     );
 };
