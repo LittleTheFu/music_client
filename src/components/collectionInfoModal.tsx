94f31374b897aec2365ca95016d597ca6d5eefd6
fuyuxiang <lttlstpd@live.com>
2020-03-19 16:05:48 +0800

    clear code

--- src/components/collectionInfoModal.tsx
+++ src/components/collectionInfoModal.tsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
-import Modal from '@material-ui/core/Modal';
+import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { useGlobal, useDispatch } from 'reactn';
+import { useGlobal } from 'reactn';
 import { Music } from '../dataInterfaces/music';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import { blue } from '@material-ui/core/colors';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
