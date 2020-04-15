143d519847c22cb9ca42a3f692bac6f9f5d97ccc
fuyuxiang <lttlstpd@live.com>
2020-04-15 10:37:20 +0800

    menu drawer

--- src/components/navDrawerComponent.tsx
+++ src/components/navDrawerComponent.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import { useGlobal } from 'reactn';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
 // const useStyles = makeStyles({
 //     list: {
@@ -20,17 +28,75 @@ import { useGlobal } from 'reactn';
 export const TemporaryDrawer: React.FC = () => {
     // const classes = useStyles();
     const [drawerOpen, setDrawerOpen] = useGlobal('drawerOpen');
+    const history = useHistory();
+    const { path, url } = useRouteMatch();
+    const [isLogin, setIsLogin] = useGlobal('isLogin');
+    const [userId, setUserId] = useGlobal('userId');
 
     return (
         <div>
             <Drawer
-                anchor="right"
+                anchor="left"
                 open={drawerOpen}
                 onClose={(): void => {
                     setDrawerOpen(false);
                 }}
             >
-                <h1>hello...........</h1>{' '}
+                <List>
+                    <ListItem
+                        button
+                        key={1}
+                        onClick={(): void => {
+                            history.push(`${url}`);
+                        }}
+                    >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={'music'} />
+                    </ListItem>
+                    <ListItem
+                        button
+                        key={2}
+                        onClick={(): void => {
+                            history.push(`${url}/mail`);
+                            console.log('MAIL CLICK');
+                        }}
+                    >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={'mail'} />
+                    </ListItem>
+                    <ListItem
+                        button
+                        key={3}
+                        onClick={(): void => {
+                            history.push(`${url}/profile`);
+                            console.log('AVATAR CLICK');
+                        }}
+                    >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={'profile'} />
+                    </ListItem>
+                    <ListItem
+                        button
+                        key={4}
+                        onClick={(): void => {
+                            setIsLogin(false);
+                            setUserId('guest');
+
+                            history.push('/login');
+                        }}
+                    >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={'logout'} />
+                    </ListItem>
+                </List>
             </Drawer>
         </div>
     );
