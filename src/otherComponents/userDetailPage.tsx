2186b4364eaa6249a757d085a2e586ecdad99b4e
fuyuxiang <lttlstpd@live.com>
2020-04-16 21:31:53 +0800

    detail page button

--- src/otherComponents/userDetailPage.tsx
+++ src/otherComponents/userDetailPage.tsx
@@ -4,10 +4,34 @@ import { getDetail } from 'service';
 import { UserDetail } from '../dataInterfaces/music';
 import Button from '@material-ui/core/Button';
 import { followUser, unfollowUser } from '../service';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MailOutlinedIcon from '@material-ui/icons/MailOutlined';
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import PeopleIcon from '@material-ui/icons/People';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        avatar: {
+            height: 80,
+            width: 80,
+        },
+        card: {
+            width: 600,
+        },
+    }),
+);
 
 export const UserDetailPage: React.FC = () => {
     const [currentClickUserId] = useGlobal('currentClickUserId');
     const [detail, setDetail] = useState<UserDetail>(null);
+
+    const classes = useStyles({});
 
     useEffect(() => {
         console.log(detail);
@@ -51,19 +75,42 @@ export const UserDetailPage: React.FC = () => {
     return (
         <div>
             {detail ? (
-                <div>
-                    ------{detail.name}--
-                    <img src={detail.avatarUrl} alt="avatar" />
-                    {detail.isFollowed ? (
-                        <Button onClick={unfollowClick} variant="contained" color="primary">
-                            unfollow
-                        </Button>
-                    ) : (
-                        <Button onClick={followClick} variant="contained" color="primary">
-                            follow
-                        </Button>
-                    )}
-                </div>
+                <Card className={classes.card}>
+                    <Grid container>
+                        <Grid item xs={2}>
+                            <img src={detail.avatarUrl} alt="avatar" className={classes.avatar} />
+                        </Grid>
+                        <Grid container item xs={10}>
+                            <Grid item xs={12}>
+                                {detail.name}
+                            </Grid>
+                            <Grid item xs={12}>
+                                <ButtonGroup color="primary" aria-label="outlined primary button group">
+                                    <IconButton>
+                                        <MailOutlinedIcon />
+                                        mail
+                                    </IconButton>
+
+                                    <IconButton>
+                                        <PeopleIcon />
+                                        follower
+                                    </IconButton>
+                                    {detail.isFollowed ? (
+                                        <IconButton onClick={unfollowClick}>
+                                            <RemoveCircleOutlineIcon />
+                                            unfollow
+                                        </IconButton>
+                                    ) : (
+                                        <IconButton onClick={followClick}>
+                                            <AddCircleOutlineIcon />
+                                            follow
+                                        </IconButton>
+                                    )}
+                                </ButtonGroup>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Card>
             ) : (
                 <div></div>
             )}
