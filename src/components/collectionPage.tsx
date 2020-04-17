8e5fa9c47177d83e074a49168dc95edcfa6dd7d8
fuyuxiang <lttlstpd@live.com>
2020-04-17 20:38:12 +0800

    show music_collections in collectionPage

--- src/components/collectionPage.tsx
+++ src/components/collectionPage.tsx
@@ -1,5 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import { getMusicCollections } from '../service';
+import { MusicCollectionsComponent } from '../components/musicCollectionsComponent';
+import { MusicCollection } from '../dataInterfaces/music';
 
 export const MusicCollectionPage: React.FC = () => {
-    return <h1>collections</h1>;
+    const [musicCollections, setMusicCollections] = useState<MusicCollection[]>([]);
+
+    const clickCollectionCover = (name: string): void => {
+        console.log('cover click');
+    };
+    const bodyClick = (name: string): void => {
+        console.log('body click');
+    };
+
+    useEffect(() => {
+        getMusicCollections((collections): void => {
+            setMusicCollections(collections);
+        }, console.log);
+    }, []);
+
+    return (
+        <MusicCollectionsComponent
+            coverClick={clickCollectionCover}
+            collections={musicCollections}
+            bodyClick={bodyClick}
+        ></MusicCollectionsComponent>
+    );
 };
