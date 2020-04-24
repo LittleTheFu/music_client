13b1be01ede80da2015a64aa9e95127227b5f87a
fuyuxiang <lttlstpd@live.com>
2020-04-24 19:07:45 +0800

    fix writeMailModal's style

--- src/mailComponents/writeMailModal.tsx
+++ src/mailComponents/writeMailModal.tsx
@@ -3,6 +3,23 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        title: {
+            textAlign: 'center',
+        },
+        inputBox: {
+            width: '100%',
+        },
+        sendButton: {
+            width: '100%',
+        },
+    }),
+);
 
 interface WriteMailModalProps {
     modalOpen: boolean;
@@ -12,8 +29,9 @@ interface WriteMailModalProps {
 
 export const WriteMailModal: React.FC<WriteMailModalProps> = (props: WriteMailModalProps) => {
     const [content, setContent] = useState('');
-
     const { modalOpen, modalClose, sendClick } = props;
+
+    const classes = useStyles({});
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
@@ -28,19 +46,34 @@ export const WriteMailModal: React.FC<WriteMailModalProps> = (props: WriteMailMo
             aria-labelledby="simple-dialog-title"
             open={modalOpen}
         >
-            <DialogTitle id="simple-dialog-title">SendMail</DialogTitle>
-            <form onSubmit={handleSubmit} noValidate autoComplete="off">
-                <TextField
-                    id="standard-basic"
-                    label="message"
-                    onChange={(e): void => {
-                        setContent(e.target.value);
-                    }}
-                />
-                <Button type="submit" variant="contained" color="primary">
-                    post
-                </Button>
-            </form>
+            <Grid container>
+                <Grid item xs={12}>
+                    <DialogTitle id="simple-dialog-title" className={classes.title}>
+                        SendMail
+                    </DialogTitle>
+                </Grid>
+                <Grid item xs={12}>
+                    <form onSubmit={handleSubmit} noValidate autoComplete="off">
+                        <Grid item xs={12}>
+                            <TextField
+                                className={classes.inputBox}
+                                id="standard-basic"
+                                label="message"
+                                multiline
+                                rows={4}
+                                onChange={(e): void => {
+                                    setContent(e.target.value);
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button type="submit" variant="contained" color="primary" className={classes.sendButton}>
+                                post
+                            </Button>
+                        </Grid>
+                    </form>
+                </Grid>
+            </Grid>
         </Dialog>
     );
 };
