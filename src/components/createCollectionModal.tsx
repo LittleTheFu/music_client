d94479d782c1ba93621db15eef5ffe4ad4785f4b
fuyuxiang <lttlstpd@live.com>
2020-04-25 15:30:24 +0800

    fix create_collection_modal's style

--- src/components/createCollectionModal.tsx
+++ src/components/createCollectionModal.tsx
@@ -1,11 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import React, { useState } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { createCollection } from '../service';
 import { MusicCollection } from '../dataInterfaces/music';
+import Grid from '@material-ui/core/Grid';
 
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        inputBox: {
+            width: '100%',
+        },
+        createButton: {
+            width: '100%',
+        },
+    }),
+);
 interface CreateCollectionModalProps {
     modalOpen: boolean;
     modalClose: () => void;
@@ -14,8 +26,9 @@ interface CreateCollectionModalProps {
 
 export const CreateCollectionModal: React.FC<CreateCollectionModalProps> = (props: CreateCollectionModalProps) => {
     const [content, setContent] = useState('');
-
     const { modalOpen, modalClose, addToCollections } = props;
+
+    const classes = useStyles({});
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
@@ -37,19 +50,23 @@ export const CreateCollectionModal: React.FC<CreateCollectionModalProps> = (prop
             }}
             open={modalOpen}
         >
-            <DialogTitle id="simple-dialog-title">Create Collection</DialogTitle>
-            <form onSubmit={handleSubmit} noValidate autoComplete="off">
-                <TextField
-                    id="standard-basic"
-                    label="name"
-                    onChange={(e): void => {
-                        setContent(e.target.value);
-                    }}
-                />
-                <Button type="submit" variant="contained" color="primary">
-                    create
-                </Button>
-            </form>
+            <Grid container>
+                <Grid item xs={12}>
+                    <form onSubmit={handleSubmit} noValidate autoComplete="off">
+                        <TextField
+                            className={classes.inputBox}
+                            id="standard-basic"
+                            label="name"
+                            onChange={(e): void => {
+                                setContent(e.target.value);
+                            }}
+                        />
+                        <Button type="submit" variant="contained" color="primary" className={classes.createButton}>
+                            create
+                        </Button>
+                    </form>
+                </Grid>
+            </Grid>
         </Dialog>
     );
 };
