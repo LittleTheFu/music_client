b0b33097a831e78e1d520a61af086abe33341fd9
fuyuxiang <lttlstpd@live.com>
2020-04-25 18:27:00 +0800

    add paddingBottom for search bar

--- src/otherComponents/searchPage.tsx
+++ src/otherComponents/searchPage.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         inputRoot: {
             paddingTop: 30,
+            paddingBottom: 30,
         },
         inputBox: {
             border: '1px solid grey',
