a7e2318c8a526eaae87629ef7bcd5046358b2bde
fuyuxiang <lttlstpd@live.com>
2020-04-26 20:53:46 +0800

    clean code

--- src/components/appBarComponent.tsx
+++ src/components/appBarComponent.tsx
@@ -35,7 +35,6 @@ export const AppBarComponent: React.FC = () => {
     const [drawerOpen, setDrawerOpen] = useGlobal('drawerOpen');
 
     const history = useHistory();
-    const { path, url } = useRouteMatch();
 
     const classes = useStyles();
 
