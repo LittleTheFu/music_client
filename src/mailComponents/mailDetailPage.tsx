ee5fc5fd47c52019a59f4ba028b13d0040db445b
fuyuxiang <lttlstpd@live.com>
2020-04-30 16:33:52 +0800

    goto user_detail_page when clicking user name in mail page

--- src/mailComponents/mailDetailPage.tsx
+++ src/mailComponents/mailDetailPage.tsx
@@ -10,11 +10,19 @@ import Button from '@material-ui/core/Button';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { BackButton } from '../otherComponents/backButton';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles(() =>
     createStyles({
         content: {
             width: '100%',
+            textIndent: 30,
+            paddingTop: 20,
+            paddingBottom: 20,
+            fontSize: 20,
+        },
+        from: {
+            fontSize: 20,
         },
         reply: {
             width: '100%',
@@ -52,6 +60,10 @@ export const MailDetailPage: React.FC = () => {
         );
     };
 
+    const fromNameClick = (fromId: number): void => {
+        history.push(`/main/userdetail/` + fromId);
+    };
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
         sendMail(mail.fromId, content, console.log, console.log);
@@ -74,15 +86,23 @@ export const MailDetailPage: React.FC = () => {
                         </Grid>
 
                         <Grid item xs={12}>
-                            <h1>{mail.toName} : </h1>
-                            <h1>{mail.content}</h1>
+                            <div className={classes.from}>
+                                From :{' '}
+                                <Link
+                                    onClick={(): void => {
+                                        fromNameClick(mail.fromId);
+                                    }}
+                                >
+                                    {mail.fromName}
+                                </Link>{' '}
+                            </div>
+                            <div className={classes.content}>{mail.content}</div>
                         </Grid>
                         <Grid item xs={12}>
                             <form onSubmit={handleSubmit} noValidate autoComplete="off">
                                 <TextField
                                     className={classes.content}
                                     id="outlined-multiline-static"
-                                    label="content"
                                     multiline
                                     rows={4}
                                     variant="outlined"
