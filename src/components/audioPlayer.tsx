fe79ee8c9af82d12952e762a5be43d3b5e4511ba
fuyuxiang <lttlstpd@live.com>
2020-05-06 19:28:35 +0800

    use <audio />

--- src/components/audioPlayer.tsx
+++ src/components/audioPlayer.tsx
@@ -2,7 +2,8 @@ let audioElement: HTMLAudioElement = null;
 
 export const getAudioPlayer = (): HTMLAudioElement => {
     if (audioElement === null) {
-        audioElement = new Audio();
+        // audioElement = new Audio();
+        audioElement = document.getElementById('audio_player') as HTMLAudioElement;
     }
 
     return audioElement;
