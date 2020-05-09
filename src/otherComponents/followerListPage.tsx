77c17797c59cf13c1bf08e14bce776965ade7a75
fuyuxiang <lttlstpd@live.com>
2020-05-09 18:30:01 +0800

    extract follower_card

--- src/otherComponents/followerListPage.tsx
+++ src/otherComponents/followerListPage.tsx
@@ -1,41 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { getUserFollowers, followUser, unfollowUser } from '../service';
 import { Follower } from '../dataInterfaces/music';
-import Card from '@material-ui/core/Card';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import { useHistory, useParams } from 'react-router-dom';
 import { BackButton } from '../otherComponents/backButton';
 import Grid from '@material-ui/core/Grid';
 import { openHint } from '../globals';
 import { useDispatch } from 'reactn';
+import { FollowerCard } from '../otherComponents/followerCard';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        avatarContainer: {
-            display: 'tableCell',
-            verticalAlign: 'middle',
-        },
-        avatar: {
-            height: 40,
-            width: 40,
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
         card: {
             padding: theme.spacing(1),
-        },
-        name: {
-            textAlign: 'center',
-        },
-        button: {
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
         },
     }),
 );
@@ -104,45 +81,12 @@ export const FollowerListPage: React.FC = () => {
     const followerElements = followers.map((f: Follower, index: number) => {
         return (
             <Grid item xs={12} sm={4} md={3} key={index} className={classes.card}>
-                <Card>
-                    <Grid container>
-                        <Grid item xs={2} className={classes.avatarContainer}>
-                            <Avatar
-                                className={classes.avatar}
-                                src={f.avatarUrl}
-                                onClick={(): void => {
-                                    avatarClick(f.id);
-                                }}
-                            ></Avatar>
-                            <div className={classes.name}>{f.name}</div>
-                        </Grid>
-                        <Grid container item xs={10}>
-                            <Grid item xs={12}>
-                                {f.isFollowed ? (
-                                    <IconButton
-                                        className={classes.button}
-                                        onClick={(): void => {
-                                            unfollowClick(f.id);
-                                        }}
-                                    >
-                                        <RemoveCircleOutlineIcon />
-                                        unfollow
-                                    </IconButton>
-                                ) : (
-                                    <IconButton
-                                        className={classes.button}
-                                        onClick={(): void => {
-                                            followClick(f.id);
-                                        }}
-                                    >
-                                        <AddCircleOutlineIcon />
-                                        follow
-                                    </IconButton>
-                                )}
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Card>
+                <FollowerCard
+                    followClick={followClick}
+                    unfollowClick={unfollowClick}
+                    avatarClick={avatarClick}
+                    follower={f}
+                ></FollowerCard>
             </Grid>
         );
     });
