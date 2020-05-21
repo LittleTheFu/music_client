8a19778a745f126363223360b2ce343006628443
fuyuxiang <lttlstpd@live.com>
2020-05-21 10:55:43 +0800

    update audio elelment

--- src/musicPlayer/audioPlayer.tsx
+++ src/musicPlayer/audioPlayer.tsx
@@ -2,8 +2,8 @@ let audioElement: HTMLAudioElement = null;
 
 export const getAudioPlayer = (): HTMLAudioElement => {
     if (audioElement === null) {
-        audioElement = new Audio();
-        // audioElement = document.getElementById('audio_player') as HTMLAudioElement;
+        // audioElement = new Audio();
+        audioElement = document.getElementById('audio_player') as HTMLAudioElement;
     }
 
     return audioElement;
