c6e1dc40dd19708eafa3f8f2d6685cf30b167a5d
fuyuxiang <lttlstpd@live.com>
2020-05-21 19:53:13 +0800

    add date_to_str function

--- src/common/common.tsx
+++ src/common/common.tsx
@@ -24,3 +24,42 @@ export const getShortContentStr = (content: string): string => {
 
     return content.substr(0, MAX_LEN) + '...';
 };
+
+// const getNumStr = (num: number, bitWidth = 2): string => {
+//     let bit = 1;
+
+//     let n = Math.floor(num / 10);
+//     while (n > 0) {
+//         bit++;
+//         n = Math.floor(n / 10);
+//     }
+
+//     let padNum = bitWidth - bit;
+
+//     if (padNum <= 0) {
+//         return num.toString();
+//     }
+
+//     let str = '';
+//     while (padNum > 0) {
+//         str += '0';
+//         padNum--;
+//     }
+//     str += num;
+
+//     return str;
+// };
+
+// export const getLocalDateStr = (date: Date): string => {
+//     const localDate = new Date(date);
+
+//     const year = localDate.getUTCFullYear();
+//     const month = localDate.getUTCMonth();
+//     const day = localDate.getUTCDay();
+
+//     const hour = localDate.getHours();
+//     const min = localDate.getMinutes();
+//     const sec = localDate.getSeconds();
+
+//     return year + '/' + month + '/' + day + ' ' + getNumStr(hour) + ':' + getNumStr(min) + ':' + getNumStr(sec);
+// };
