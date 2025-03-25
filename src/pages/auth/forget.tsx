2f7d325ba77241599e033c5c913871b387b04ef6
LittleTheFu <lttlstpd@live.com>
2025-03-25 11:35:10 +0800

    save

--- src/pages/auth/forget.tsx
+++ src/pages/auth/forget.tsx
@@ -12,6 +12,7 @@ import Container from '@mui/material/Container';
 import { isValidUserName, getUsernameHelpText, isValidEmail, getEmailHelpText } from '../../common/common';
 import { postForgetPassword } from '../../common/service';
 import { RetMsgObj } from '../../common/interface';
+import { Link } from 'react-router-dom'; // 导入 Link 组件
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -84,11 +85,61 @@ export const ForgetPasswordPage: React.FC = () => {
                     <Button type="submit" variant="contained" color="primary">
                         submit
                     </Button>
+                </form>
+            </div>
+        </Container>
+    );
+};
+
+export const ForgetPage: React.FC = () => {
+    const [user, setUser] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('input your name and email');
+
+    const classes = useStyles({});
+
+    const resolve = (r: RetMsgObj): void => {
+        setMessage(r.msg);
+    };
+
+    const reject = (e: Error): void => {
+        setMessage(e.message);
+    };
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        postForgetPassword(user, email, resolve, reject);
+    }
+
+    return (
+        <div>
+            <h1 className={classes.msg}>{message}</h1>
+            <div className={classes.paper}>
+                <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
+                    <TextField
+                        error={!isValidUserName(user)}
+                        id="name"
+                        label="user"
+                        onChange={(e): void => setUser(e.target.value)}
+                        helperText={getUsernameHelpText()}
+                    />
+                    <TextField
+                        error={!isValidEmail(email)}
+                        autoComplete="on"
+                        id="email"
+                        label="email"
+                        type="email"
+                        onChange={(e): void => setEmail(e.target.value)}
+                        helperText={getEmailHelpText()}
+                    />
+                    <Button type="submit" variant="contained" color="primary">
+                        submit
+                    </Button>
                     <Button component={Link} to="/login" type="submit" variant="contained" color="primary">
                         go to login page
                     </Button>
                 </form>
             </div>
-        </Container>
+        </div>
     );
 };
