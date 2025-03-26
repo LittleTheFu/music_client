60a23101dfe1210c534bfaa3228757b457c24210
LittleTheFu <lttlstpd@live.com>
2025-03-26 10:12:12 +0800

    SAVE

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // 引入 createRoot 方法
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+// 使用 createRoot 方法创建根节点
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// 使用 root.render 方法渲染应用
+root.render(<React.StrictMode><App /></React.StrictMode>);
