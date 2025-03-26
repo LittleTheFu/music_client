15b2abaa1e5dfb27fe39b9fb01f580a1e2e82b40
LittleTheFu <lttlstpd@live.com>
2025-03-26 10:07:17 +0800

    SAVE

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// 修改前
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// 修改后
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
